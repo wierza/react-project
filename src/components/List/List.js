f44e99f1606f3efe0388d36cc4fd05471c35ace3
wierza <dawid@wierzycki.pl>
2023-06-28 22:02:43 +0200

    Add action

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,8 +1,58 @@
 import styles from './List.module.scss';
 import Column from '../Column/Column.js';
+import { useState } from 'react';
+import shortid from 'shortid';
+import ColumnForm from '../ColumnForm/ColumnForm.js'
 
 
 const List = () => {
+    
+    const [columns, setColumns] = useState([
+        {
+            id: 1,
+            title: 'Books',
+            icon: 'book',
+            cards: [
+                { id: 1, title: 'This is Going to Hurt' },
+                { id: 2, title: 'Interpreter of Maladies' }
+            ]
+        },
+        {
+            id: 2,
+            title: 'Movies',
+            icon: 'film',
+            cards: [
+                { id: 1, title: 'Harry Potter' },
+                { id: 2, title: 'Star Wars' }
+            ]
+        },
+        {
+            id: 3,
+            title: 'Games',
+            icon: 'gamepad',
+            cards: [
+                { id: 1, title: 'The Witcher' },
+                { id: 2, title: 'Skyrim' }
+            ]
+        }
+    ]);
+    
+    const addColumn = newColumn => {
+        setColumns([...columns, { id: shortid(), title: newColumn.title, icon: newColumn.icon, cards: []}]);
+    };
+
+    const addCard = (newCard, columnId) => {
+        const columnsUpdated = columns.map(column => {
+            if(column.id === columnId)
+                return { ...column, cards: [...column.cards, { id: shortid(), title: newCard.title }]}
+            else
+                return column
+        })
+    
+        setColumns(columnsUpdated);
+    
+    };
+
     return (
        <div className={styles.list}>
             <header className={styles.header}>
@@ -10,9 +60,8 @@ const List = () => {
                 <p className={styles.description}>Interesting things I want to check out!</p>
             </header>
             <section className={styles.columns}>
-                <Column title="Books" icon="book" />
-                <Column title="Movies" icon="film" />
-                <Column title="Games" icon="gamepad"/>
+                {columns.map(column => <Column key={column.id} id={column.id} title={column.title} icon={column.icon} cards={column.cards} action={addCard} />)}
+                <ColumnForm action={addColumn} />
             </section>
         </div>
     );
--- src/components/TextInput/TextInput.js
+++ src/components/TextInput/TextInput.js
@@ -2,7 +2,7 @@ import styles from './TextInput.module.scss';
 import 'font-awesome/css/font-awesome.min.css';
 
 const TextInput = props => {
-    return (<input className={styles.input} placeholder={props.placeholder} type="text" />);
+    return (<input className={styles.input} value={props.value} onChange={props.onChange} placeholder={props.placeholder} type="text" />);
 };
 
 export default TextInput;
