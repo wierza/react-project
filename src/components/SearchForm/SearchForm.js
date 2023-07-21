3f6d639a1f8ddc7bfa2005b03255280964ca6e66
wierza <dawid@wierzycki.pl>
2023-07-21 21:28:35 +0200

    Add SearchForm

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,11 +1,21 @@
 import styles from './SearchForm.module.scss';
 import TextInput from '../TextInput/TextInput.js';
 import Button from '../Button/Button.js';
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
 
 const SearchForm = () => {
+    const dispatch = useDispatch();
+    const [searchValue, setSearchValue] = useState('');
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        dispatch({type: 'UPDATE_SEARCHSTRING', payload: searchValue});
+    };
+
     return (
-        <form className={styles.searchForm}>
-            <TextInput placeholder="Search..." />
+        <form className={styles.searchForm} onSubmit={handleSubmit}>
+            <TextInput placeholder="Search..." onChange={e => setSearchValue(e.target.value)}/>
             <Button>
                 <span className="fa fa-search" />
             </Button>
--- src/redux/store.js
+++ src/redux/store.js
@@ -8,6 +8,8 @@ const reducer = (state, action) => {
         return { ...state, columns: [...state.columns, { ...action.payload, id: shortid() }]};
       case 'ADD_CARD':
         return {...state, cards: [...state.cards, { ...action.payload, id: shortid() }]};
+      case 'UPDATE_SEARCHING':
+        return {...state, searchString: action.palyload};
       default:
         return state;
     }
